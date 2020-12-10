2ff308d3b352975f6740a7f1db2b4ef73efd922e
Paola Lopez <paola.lopez1@yahoo.com>
2020-12-09 18:09:59 -0800

    fixed errors, completed the first few tasks

--- SQL_Challenge.sql
+++ SQL_Challenge.sql
@@ -1,10 +1,3 @@
-DROP TABLE IF EXISTS "titles";
-DROP TABLE IF EXISTS "employees";
-DROP TABLE IF EXISTS "salaries";
-DROP TABLE IF EXISTS "departments";
-DROP TABLE IF EXISTS "dept_emp";
-DROP TABLE IF EXISTS "dept_manager";
-
 CREATE TABLE "titles" (
     "title_id" varchar   NOT NULL,
     "title" varchar   NOT NULL,
@@ -29,10 +22,9 @@ CREATE TABLE "employees" (
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
     "salary" money   NOT NULL,
-
-    CONSTRAINT "uc_salaries_emp_no" UNIQUE (
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
-    )
+     )
 );
 
 CREATE TABLE "departments" (
@@ -46,25 +38,17 @@ CREATE TABLE "departments" (
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar   NOT NULL,
-
-    CONSTRAINT "uc_dept_emp_emp_no" UNIQUE (
-        "emp_no"
-    ),
-    CONSTRAINT "uc_dept_emp_dept_no" UNIQUE (
-        "dept_no"
-    )
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" varchar   NOT NULL,
     "emp_no" int   NOT NULL,
-
-    CONSTRAINT "uc_dept_manager_dept_no" UNIQUE (
-        "dept_no"
-    ),
-    CONSTRAINT "uc_dept_manager_emp_no" UNIQUE (
-        "emp_no"
-    )
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
@@ -83,3 +67,44 @@ ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no_emp_no" FOREI
 REFERENCES "dept_emp" ("dept_no", "emp_no");
 
 
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+FROM employees
+INNER JOIN salaries ON
+employees.emp_no = salaries.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986';
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT 
+
+
+
+
+
+dept_manager.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+	
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
